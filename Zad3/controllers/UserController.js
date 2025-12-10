563ff3f438d2a532a574db593d6087af0d8be8ee
Remko23 <r-tomecki.live@wp.pl>
2025-12-10 15:44:32 +0100

    Fixes

--- Zad3/controllers/UserController.js
+++ Zad3/controllers/UserController.js
@@ -2,8 +2,8 @@ const User = require('../models/user');
 const { StatusCodes } = require('http-status-codes');
 const jwt = require('jsonwebtoken');
 
-const SECRET_KEY = 'default_secret_key';
-const REFRESH_SECRET_KEY = 'default_refresh_secret_key';
+const SECRET_KEY = 'my_secret_key';
+const REFRESH_SECRET_KEY = 'my_refresh_secret_key';
 
 const TOKEN_EXPIRATION = '1h'; 
 const REFRESH_TOKEN_EXPIRATION = '7d';
